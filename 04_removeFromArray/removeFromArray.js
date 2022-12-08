7d9e7f26d2e6505c01700559f664421c652413d0
Sean <73760350+SeanGrimes44@users.noreply.github.com>
2022-12-08 10:57:10 -0500

    Complete 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(items, ...itemsToRemove) {
+    for (let i = 0; i < itemsToRemove.length; i++) {
+        let removeIndex = items.indexOf(itemsToRemove[i]);
+        if (removeIndex >= 0) {
+            items.splice(removeIndex, 1);
+        }
+    }
+    return items;
 };
 
 // Do not edit below this line
