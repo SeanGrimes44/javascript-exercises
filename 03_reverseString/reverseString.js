0ef1e8587ceb0f33a22e0dd21d2c778f13b73a2d
Sean <73760350+SeanGrimes44@users.noreply.github.com>
2022-12-08 10:38:08 -0500

    Complete 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(stringToReverse) {
+    let answer = "";
+    for (let i = stringToReverse.length; i >= 0; i--) {
+        answer += stringToReverse.charAt(i);
+    }
+    return answer;
 };
 
 // Do not edit below this line
