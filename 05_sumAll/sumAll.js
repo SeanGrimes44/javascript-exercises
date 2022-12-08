046e123b28a0b254d04313ec248725cab670aafd
Sean <73760350+SeanGrimes44@users.noreply.github.com>
2022-12-08 11:04:40 -0500

    Complete 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
-
+const sumAll = function(firstNumber, secondNumber) {
+    if (typeof(firstNumber) != "number" || typeof(secondNumber) !== "number") {
+        return "ERROR";
+    }
+    if (firstNumber < 0 || secondNumber < 0) {
+        return "ERROR";
+    }
+    let startNumber = 0;
+    let endNumber = 0;
+    if (firstNumber < secondNumber) {
+        startNumber = firstNumber;
+        endNumber = secondNumber
+    }
+    else {
+        startNumber = secondNumber;
+        endNumber = firstNumber;
+    }
+    let answer = 0;
+    for (let i = startNumber; i <= endNumber; i++) {
+        answer += i;
+    }
+    return answer;
 };
 
 // Do not edit below this line
