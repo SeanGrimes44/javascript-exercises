d6728988daaf69c08257d8f98050df2d39d6ba12
Sean <73760350+SeanGrimes44@users.noreply.github.com>
2022-12-08 10:30:26 -0500

    Complete 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(stringToRepeat, numberOfTimes) {
+    if (numberOfTimes < 0) {
+        return "ERROR";
+    }
+    
+    let finalString = "";
+    for (let i = 0; i < numberOfTimes; i++) {
+        finalString += stringToRepeat;
+    }
+    return finalString;
 };
 
 // Do not edit below this line
