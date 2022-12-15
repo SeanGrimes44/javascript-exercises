124d6d339ad88172178e69547a37d25a53ad27c0
Sean <73760350+SeanGrimes44@users.noreply.github.com>
2022-12-15 11:29:00 -0500

    Complete 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+    let aInt = Number(a);
+    let answer = 0;
+    if (aInt < 0) {
+        return "OOPS";
+    }
+    if (aInt <= 2) {
+        return 1;
+    }
+    let one = 1;
+    let two = 1;
+    let top = 0;
+    for (let i = 2; i < aInt; i++) {
+        top = one + two;
+        one = two;
+        two = top;
+    }
+    return top;
 };
 
 // Do not edit below this line
