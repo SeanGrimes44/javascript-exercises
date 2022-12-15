0db8b08170ff2b0535887f178dbd64c0f0017fe0
Sean <73760350+SeanGrimes44@users.noreply.github.com>
2022-12-15 11:13:32 -0500

    Complete 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (a) {
+    a = a.toLowerCase();
+    let regex = /[\W_]/g;
+    a = a.replace(regex, "");
+    console.log(a);
+    let reverseString = "";
+    for (let i = a.length-1; i >= 0; i--) {
+        reverseString += a[i];
+    }
+    console.log(reverseString)
 
+    if (reverseString == a) {
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
